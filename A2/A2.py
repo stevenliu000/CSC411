9168e09ffad45eea29b825846a0720c4f319b0a2
stevenliu000 <wxabc147@hotmail.com>
2018-02-09 03:14:42 -0500

    Steven working on part1-3

--- A2/A2.py
+++ A2/A2.py
@@ -18,6 +18,7 @@ from scipy.io import loadmat
 
 ##############################################################################
 ################################# Import Data ################################
+
 def importData():
 	M = loadmat("mnist_all.mat")
 	X_train = np.empty((0,784))
@@ -27,26 +28,28 @@ def importData():
 
 	for i in range(0,10):
 		name = "train" + str(i)
-		X_train = np.hstack(X_train, M[name]/255.0)
+		X_train = np.vstack((X_train, M[name]/255.0))
 		Y = np.zeros((10,M[name].shape[0]))
 		Y[i,:] = 1
-		Y_train = np.vstack(Y_train, Y)
+		Y_train = np.hstack((Y_train, Y))
 
 		name = "test" + str(i)
-		X_test = np.vstack(X_test, M[name]/255.0)
+		X_test = np.hstack((X_test, M[name]/255.0))
 		Y = np.zeros((10,M[name].shape[0]))
 		Y[i,:] = 1
-		Y_test = np.vstack(Y_test, Y)
+		Y_test = np.hstack((Y_test, Y))
 
 	return X_train, Y_train, X_test, Y_train
 
 ##############################################################################
 #################################### Part1 ###################################
+
 def part1():
 	return
 
 ##############################################################################
 #################################### Part2 ###################################
+
 def softmax(Y):
     '''Return the output of the softmax function for the matrix of output y. y
     is an NxM matrix where N is the number of outputs for a single case, and M
@@ -54,7 +57,7 @@ def softmax(Y):
     return exp(Y)/tile(sum(exp(y),0), (len(y),1))
 
 def forward(X, W0, b0):
-	O = dot(W0.T, X) + b0
+    O = dot(W0.T, X) + b0
     output = softmax(O)
     return O, output
 
@@ -79,16 +82,34 @@ def Gradient(X, Y, W0, b0):
 	O, output = forward(X, W0, b0)
 	return dot((output - Y), X)
 
-def finite_diff(f, x, y, theta, row, col, h):
+def finite_diff(CostFunction, X, Y, W0, b0, row, col, h):
     #function for calculating one component of gradient 
     #using finite-difference approximation
-    theta_h = np.copy(theta)
-    theta_h[row,col] = theta_h[row,col] + h
-    return (f(x, y ,theta_h) - f(x, y, theta))/h
+    W0_h = np.copy(W0)
+    W0_h[row,col] = W0_h[row,col] + h
+    return (CostFunction(Y, X, W0_h) - CostFunction(X, Y, W0))/h
+
+def part3b():
+    np.random.seed(1)
+    W0 = np.random.normal(scale = 0.0001, size = (10,784))
+    b0 = np.random.normal(scale = 0.0001, size = 10)
+    row_ = np.random.randint(0,10,7)
+    col_ = np.random.randint(0,784,7)
+    h = 10**(-6)
+    gradient = Gradient(X_train, Y_train, W0, b0)
+    for i in range(7):
+        print(abs(finite_diff(f_multi, X_train, Y_train, W0, b0, row_[i], \
+        	col_[i],h) - gradient[row_[i], col_[i]]))
+
 ##############################################################################
 #################################### Part4 ###################################
 
+def part4():
+	return
 
+##############################################################################
+#################################### Main ####################################
 
 if __name__ == "__main__":
 	X_train, Y_train, X_test, Y_train = importData()
+	part3()
