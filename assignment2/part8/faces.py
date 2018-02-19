6026dfd901d0953a7124f87dd0c9d40bc160108a
stevenliu000 <wxabc147@hotmail.com>
2018-02-19 01:41:16 -0500

    ad

--- assignment2/part8/faces.py
+++ assignment2/part8/faces.py
@@ -12,9 +12,13 @@ import os
 from scipy.ndimage import filters
 import urllib
 import hashlib
+from torch.autograd import Variable
+import torch
+import torch.utils.data as data_utils
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Part1
+##############################################################################
+######################### Download and Process Data ##########################
+
 def rgb2gray(rgb):
     '''Return the grayscale version of the RGB image rgb as a 2D numpy array
     whose range is 0..1
@@ -135,43 +139,65 @@ def Download():
 
     return
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Part2
-def get_names():
-    NameList = []
-
-    for i in ['actors', 'actresses']:
-        act_type = i
-        dataset_path = dirpath + "/facescrub_" + act_type + "_cropped/"
-        for root, dirs, files in os.walk(dataset_path):
-            for filename in files:
-                temp = filename.rfind('.') - 1
-                while filename[temp].isdigit():
-                    temp -= 1
-                if filename[:temp + 1] not in NameList:
-                    NameList.append(filename[:temp + 1])
-
-    return NameList
-
-
-def part2(s = 0):
-    act = ['Alec Baldwin', 'Bill Hader', 'Daniel Radcliffe', 'Gerard Butler', 'Michael Vartan', \
-           'Steve Carell', 'America Ferrera', 'Angie Harmon', 'Fran Drescher', 'Kristin Chenoweth', \
-           'Lorraine Bracco', 'Peri Gilpin']
-    act_rawdata = {}
-    act_data = {}
-    for i in act:
-        act_rawdata[i] = np.empty((1024,0))
-        act_data[i] = [0, 0, 0]
-
-    for j in ['actors', 'actresses']:
-        act_type = j
+def Resize(S, grayed = True):
+    '''
+    input: a turple of size. e.g. (32,32)
+    Resize the cropped images, and store them into XXX_size(_grayed)
+    hardcode_list is a list that contains filenames 
+    '''
+    hardcode_list = ['Kristin Chenoweth71.jpg']
+    for act_type in ['actors', 'actresses']:
+        if grayed:
+            store_path = dirpath + "/facescrub_%s_%i_grayed/" %(act_type, S[0])
+        else:
+            store_path = dirpath + "/facescrub_%s_%i/" %(act_type, S[0])
+        os.makedirs(store_path)
         dataset_path = dirpath + "/facescrub_" + act_type + "_cropped/"
         for root, dirs, files in os.walk(dataset_path):
             dirs.sort()
             files.sort()
             for filename in files:
-                im = imread(dataset_path + filename)
+                if filename not in hardcode_list:
+                    im = imread(dataset_path + filename, mode='RGB')
+                    resized = imresize(im, S)
+                    if grayed:
+                        if im.ndim == 3:
+                            resized = rgb2gray(resized)
+                        plt.imsave(store_path + filename, resized, cmap = cm.gray)
+                    else:
+                        
+                        plt.imsave(store_path + filename, resized)
+
+##############################################################################
+################################### Get Data #################################
+
+def get_data(S, act, grayed = True, s = 0):
+    '''
+    input: S is the size of imgaes, grayed is whether the images are grayed, s is the random seed
+
+    Getting validation sets and training sets.
+
+    return act_data which is a dictionary whose key is the name of actor/actress, value is the 3-element long array that the first element is a array that contains the training set, second one contains validation set, third one contains test set.
+    '''
+    act_rawdata = {}
+    act_data = {}
+    for i in act:
+        act_rawdata[i] = np.empty((S[0]*S[0],0))
+        act_data[i] = [0, 0, 0]
+
+    for act_type in ['actors', 'actresses']:
+        if grayed:
+            dataset_path = dirpath + "/facescrub_%s_%i_grayed/" %(act_type, S[0])
+        else:
+            dataset_path = dirpath + "/facescrub_%s_%i/" %(act_type, S[0])
+        for root, dirs, files in os.walk(dataset_path):
+            dirs.sort()
+            files.sort()
+            for filename in files:
+                if grayed:
+                    im = imread(dataset_path + filename, mode = 'L')
+                else:
+                    im = imread(dataset_path + filename, mode = 'RGB')
                 im = im/255.
                 im = np.array([im.flatten()]).T
                 if np.amax(im) > 1.0 or np.amin(im) < 0. or np.isnan(im).any() or np.isinf(im).any():
@@ -186,20 +212,129 @@ def part2(s = 0):
     for i in act:
         act_rawdata[i] = act_rawdata[i][:,np.random.permutation(act_rawdata[i].shape[1])]
 
-    # act_data is a dictionary whose key is the name of actor/actress, value is the 3-element long array that the first
-    # element is a array that contains the training set, second one contains validation set, third one
-    # contains test set.
+    # construct output act_data
     for i in act:
-        act_data[i][0] = act_rawdata[i][:, :min(70, act_rawdata[i].shape[1] - 20)]
+        act_data[i][0] = act_rawdata[i][:, :min(70, act_rawdata[i].shape[1] - 30)]
         act_data[i][1] = act_rawdata[i][:, -20:-10]
-        act_data[i][2] = act_rawdata[i][:, -10:]
+        act_data[i][2] = act_rawdata[i][:, -20:]
+
     return act_data
 
+##############################################################################
+################################### Pytorch ##################################
+
+def Run(S, batch_size, nEpoch, alpha = 1e-2, grayed = True, s = 0):
+
+    act1 = ['Lorraine Bracco', 'Peri Gilpin', 'Angie Harmon', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
+
+    # set torch seed
+    torch.manual_seed(0)
+
+    # get data
+    act_data = get_data(S, act1, grayed = grayed, s = s)
+
+    # construct training set, validation set, test set
+    
+    X_train = np.empty((S[0]*S[0], 0))
+    Y_train = np.empty((len(act1), 0))
+    X_vali = np.empty((S[0]*S[0], 0))
+    Y_vali = np.empty((len(act1), 0))
+    X_test = np.empty((S[0]*S[0], 0))
+    Y_test = np.empty((len(act1), 0))
+    for i in range(len(act1)):
+        temp = np.zeros((len(act1), act_data[act1[i]][0].shape[1]))
+        temp[i, :] = 1
+        X_train = np.hstack((X_train, act_data[act1[i]][0]))
+        Y_train = np.hstack((Y_train, temp))
+
+        temp = np.zeros((len(act1), act_data[act1[i]][1].shape[1]))
+        temp[i, :] = 1
+        X_vali = np.hstack((X_vali, act_data[act1[i]][1]))
+        Y_vali = np.hstack((Y_vali, temp))
+
+        temp = np.zeros((len(act1), act_data[act1[i]][2].shape[1]))
+        temp[i, :] = 1
+        X_test = np.hstack((X_test, act_data[act1[i]][2]))
+        Y_test = np.hstack((Y_test, temp))
+
+    X_train = X_train.T
+    Y_train = Y_train.T
+    X_vali = X_vali.T
+    Y_vali = Y_vali.T
+    X_test = X_test.T
+    Y_test = Y_test.T
+
+    # define torch variablaes
+    dtype_float = torch.FloatTensor
+    dtype_long = torch.LongTensor
+
+    X_TRAIN = Variable(torch.from_numpy(X_train), requires_grad=False).type(dtype_float)
+    Y_CLASSES = Variable(torch.from_numpy(np.argmax(Y_train, 1)), requires_grad=False).type(dtype_long)
+    X_VALI = Variable(torch.from_numpy(X_vali), requires_grad=False).type(dtype_float)
+    X_TEST = Variable(torch.from_numpy(X_test), requires_grad=False).type(dtype_float)
+    TRAIN = data_utils.TensorDataset(X_TRAIN, Y_CLASSES)
+    dataloader = data_utils.DataLoader(TRAIN, batch_size=batch_size, shuffle=True)
+
+    # define model
+    model = torch.nn.Sequential(
+        torch.nn.Linear(S[0]*S[0], 12),
+        torch.nn.ReLU(),
+        torch.nn.Linear(12, len(act1)),
+        torch.nn.Softmax()
+    )
+
+    # define loss function
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    # initialize weight
+    model[0].weight.data.normal_(0.0,0.01)
+    model[2].weight.data.normal_(0.0,0.01)
+    model[0].bias.data.normal_(0.0,0.01)
+    model[2].bias.data.normal_(0.0,0.01)
+
+    # define optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=alpha)
+
+    # gradient descent
+    num_epoch = []
+    perf_train = []
+    perf_vali = []
+    perf_test = []
+
+    for Epoch in range(nEpoch):
+        for iMinibatch, Minibatch in enumerate(dataloader):
+            x, y = Minibatch
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            model.zero_grad()  # Zero out the previous gradient computation
+            loss.backward()    # Compute the gradient
+            optimizer.step()   # Use the gradient information to make a step
+
+        if epoch % 5 == 0:
+            print Epoch
+            num_epoch.append(Epoch)
+            perf_train.append(np.mean(np.argmax(model(X_TRAIN).data.numpy(), 1) == np.argmax(Y_train, 1)))
+            perf_vali.append(np.mean(np.argmax(model(X_VALI).data.numpy(), 1) == np.argmax(Y_vali, 1)))
+            perf_test.append(np.mean(np.argmax(model(X_TEST).data.numpy(), 1) == np.argmax(Y_test, 1)))
+
+    fig = plt.figure(80)
+    plt.plot(num_epoch, perf_train,'-1', label = 'training set')
+    plt.plot(num_epoch, perf_vali,'-2', label = 'validation set')
+    plt.plot(num_epoch, perf_test,'-3', label = 'test set')
+    plt.xlabel("Number of epoches")
+    plt.ylabel('Performance')
+    plt.legend(loc = "upper right")
+    fig.savefig(dirpath + '/part8_%i_%i_%i_%070.07f.jpg'%(S, batch_size, nEpoch, alpha))
+    plt.show()
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Main Block
+##############################################################################
+################################## Main Block ################################
+
 if __name__ == "__main__":
     #os.chdir(os.path.dirname(__file__))
     dirpath = os.getcwd()
-    Download()
+    #Download()
+    #Resize((32,32))
+    Run((32,32), 10, 2000, alpha = 1e-4, grayed = True, s = 0)
+
