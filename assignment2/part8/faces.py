2133245c9b8e7fc4b29f7888f672ea8d82713b5d
stevenliu000 <wxabc147@hotmail.com>
2018-02-21 22:06:35 -0500

    finish part9

--- assignment2/part8/faces.py
+++ assignment2/part8/faces.py
@@ -17,7 +17,7 @@ import torch
 import torch.utils.data as data_utils
 
 ##############################################################################
-######################### Download and Process Data ##########################
+#################### Part 8: Download and Process Data #######################
 
 def rgb2gray(rgb):
     '''Return the grayscale version of the RGB image rgb as a 2D numpy array
@@ -163,13 +163,17 @@ def Resize(S, grayed = True):
                     if grayed:
                         if im.ndim == 3:
                             resized = rgb2gray(resized)
+                        elif im.ndim == 2:
+                            resized = resized/255.
+                        else:
+                            continue
                         plt.imsave(store_path + filename, resized, cmap = cm.gray)
                     else:
-                        
+                        resized = resized/255.
                         plt.imsave(store_path + filename, resized)
 
 ##############################################################################
-################################### Get Data #################################
+############################### Part 8: Get Data #############################
 
 def get_data(S, act, grayed = True, s = 0):
     '''
@@ -178,6 +182,8 @@ def get_data(S, act, grayed = True, s = 0):
     Getting validation sets and training sets.
 
     return act_data which is a dictionary whose key is the name of actor/actress, value is the 3-element long array that the first element is a array that contains the training set, second one contains validation set, third one contains test set.
+    '''
+    
     '''
     act_rawdata = {}
     act_data = {}
@@ -194,10 +200,7 @@ def get_data(S, act, grayed = True, s = 0):
             dirs.sort()
             files.sort()
             for filename in files:
-                if grayed:
-                    im = imread(dataset_path + filename, mode = 'L')
-                else:
-                    im = imread(dataset_path + filename, mode = 'RGB')
+                im = imread(dataset_path + filename)
                 im = im/255.
                 im = np.array([im.flatten()]).T
                 if np.amax(im) > 1.0 or np.amin(im) < 0. or np.isnan(im).any() or np.isinf(im).any():
@@ -220,21 +223,85 @@ def get_data(S, act, grayed = True, s = 0):
 
     return act_data
 
+    '''
+
+
+
+    act_rawdata = {}
+    act_data = {}
+    for i in act:
+        act_rawdata[i] = np.empty((S[0]*S[0],0))
+        act_data[i] = [0, 0, 0]
+
+    for act_type in ['actors', 'actresses']:
+        if grayed:
+            dataset_path = dirpath + "/facescrub_%s_%i_grayed/" %(act_type, S[0])
+        else:
+            dataset_path = dirpath + "/facescrub_%s_%i/" %(act_type, S[0])
+        for root, dirs, files in os.walk(dataset_path):
+            dirs.sort()
+            files.sort()
+            for filename in files:
+                im = imread(dataset_path + filename)
+                im = im/255.
+                im = np.array([im[:,:,0].flatten()]).T
+                if np.amax(im) > 1.0 or np.amin(im) < 0. or np.isnan(im).any() or np.isinf(im).any():
+                    continue
+                else:
+                    for i in act:
+                        if i in filename:
+                            act_rawdata[i] = np.hstack((act_rawdata[i], im))
+
+    # randomly shuffle act_rawdata
+    np.random.seed(s)
+    for i in act:
+        act_rawdata[i] = act_rawdata[i][:,np.random.permutation(act_rawdata[i].shape[1])]
+
+    # act_data is a dictionary whose key is the name of actor/actress, value is the 3-element long array that the first
+    # element is a array that contains the training set, second one contains validation set, third one
+    # contains test set.
+    for i in act:
+        act_data[i][0] = act_rawdata[i][:, :min(70, act_rawdata[i].shape[1] - 30)]
+        act_data[i][1] = act_rawdata[i][:, -30:-20]
+        act_data[i][2] = act_rawdata[i][:, -20:]
+    return act_data
+
 ##############################################################################
-################################### Pytorch ##################################
+############################### Part 8: Pytorch ##############################
 
 def Run(S, batch_size, nEpoch, alpha = 1e-2, grayed = True, s = 0):
+    '''
+    input: S is a tuple of size
+            batch_size is the size of minbatch used in gradient descent
+            nEpoch is the number of epoch it will run
+            alpha is the learning rate
+            grayed indicates whether the images are grayed
+            s is the random seed used
+    
+    This function:
+        1. initialize torch seed
+        2. get training, validation, and training sets by calling function get_data
+        3. format training, validation, and training sets
+        4. define torch variablaes
+        5. initilize dataloader which will be used in minibatch gradient descent
+        6. define model and loss function
+        7. initialize weight
+        8. define optimizer
+        9. minibatch gradient descent and calculate performance in every 5 epoches
+        10. plot learning rate
 
+    return pytorch model
+
+    '''
     act1 = ['Lorraine Bracco', 'Peri Gilpin', 'Angie Harmon', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
 
     # set torch seed
-    torch.manual_seed(0)
+    torch.manual_seed(s)
 
     # get data
     act_data = get_data(S, act1, grayed = grayed, s = s)
 
     # construct training set, validation set, test set
-    
     X_train = np.empty((S[0]*S[0], 0))
     Y_train = np.empty((len(act1), 0))
     X_vali = np.empty((S[0]*S[0], 0))
@@ -264,21 +331,26 @@ def Run(S, batch_size, nEpoch, alpha = 1e-2, grayed = True, s = 0):
     X_test = X_test.T
     Y_test = Y_test.T
 
+    # pack X_train and Y_train into one variables
+    Y_classes = np.argmax(Y_train, 1).reshape((X_train.shape[0],1))
+    XY_train = np.hstack((X_train, Y_classes))
+
     # define torch variablaes
     dtype_float = torch.FloatTensor
     dtype_long = torch.LongTensor
 
     X_TRAIN = Variable(torch.from_numpy(X_train), requires_grad=False).type(dtype_float)
-    Y_CLASSES = Variable(torch.from_numpy(np.argmax(Y_train, 1)), requires_grad=False).type(dtype_long)
     X_VALI = Variable(torch.from_numpy(X_vali), requires_grad=False).type(dtype_float)
     X_TEST = Variable(torch.from_numpy(X_test), requires_grad=False).type(dtype_float)
-    TRAIN = data_utils.TensorDataset(X_TRAIN, Y_CLASSES)
-    dataloader = data_utils.DataLoader(TRAIN, batch_size=batch_size, shuffle=True)
+
+    # initilize dataloader which will be used in minibatch gradient descent
+    dataloader = data_utils.DataLoader(XY_train, batch_size=batch_size, shuffle=True)
+
 
     # define model
     model = torch.nn.Sequential(
         torch.nn.Linear(S[0]*S[0], 12),
-        torch.nn.ReLU(),
+        torch.nn.Tanh(),
         torch.nn.Linear(12, len(act1)),
         torch.nn.Softmax()
     )
@@ -303,29 +375,52 @@ def Run(S, batch_size, nEpoch, alpha = 1e-2, grayed = True, s = 0):
 
     for Epoch in range(nEpoch):
         for iMinibatch, Minibatch in enumerate(dataloader):
-            x, y = Minibatch
+            x = Variable(Minibatch[:,:-1], requires_grad = False).type(dtype_float)
+            y = Variable(Minibatch[:,-1], requires_grad = False).type(dtype_long)
             y_pred = model(x)
             loss = loss_fn(y_pred, y)
             model.zero_grad()  # Zero out the previous gradient computation
             loss.backward()    # Compute the gradient
             optimizer.step()   # Use the gradient information to make a step
 
-        if epoch % 5 == 0:
-            print Epoch
+        # calculate the performance
+        if Epoch % 10 == 0:
+            print "The current epoch is %i"%Epoch
             num_epoch.append(Epoch)
             perf_train.append(np.mean(np.argmax(model(X_TRAIN).data.numpy(), 1) == np.argmax(Y_train, 1)))
             perf_vali.append(np.mean(np.argmax(model(X_VALI).data.numpy(), 1) == np.argmax(Y_vali, 1)))
             perf_test.append(np.mean(np.argmax(model(X_TEST).data.numpy(), 1) == np.argmax(Y_test, 1)))
 
+
+    # plot learning curve
     fig = plt.figure(80)
     plt.plot(num_epoch, perf_train,'-1', label = 'training set')
     plt.plot(num_epoch, perf_vali,'-2', label = 'validation set')
     plt.plot(num_epoch, perf_test,'-3', label = 'test set')
-    plt.xlabel("Number of epoches")
+    plt.title('Part 8: Learning Rate')
+    plt.xlabel('Number of epoches')
     plt.ylabel('Performance')
-    plt.legend(loc = "upper right")
-    fig.savefig(dirpath + '/part8_%i_%i_%i_%070.07f.jpg'%(S, batch_size, nEpoch, alpha))
+    plt.legend(loc = 'upper right')
+    fig.savefig(dirpath + '/part8_%i_%i_%i_%07.07f.jpg'%(S[0], batch_size, nEpoch, alpha))
     plt.show()
+
+    return model
+
+##############################################################################
+#################################### Part 9 ##################################
+
+def part9(S):
+    '''
+    input: S is a tuple of size
+
+    plot the image of weights generated by part 8
+    '''
+
+    for i in range(12):
+        fig = plt.figure(90+i)
+        plt.imshow(model[0].weight.data.numpy()[10, :].reshape(S), cmap=plt.cm.coolwarm)
+        fig.savefig(dirpath + '/part9_%i.jpg'%i)
+        plt.show()
 
 
 ##############################################################################
@@ -336,5 +431,6 @@ if __name__ == "__main__":
     dirpath = os.getcwd()
     #Download()
     #Resize((32,32))
-    Run((32,32), 10, 2000, alpha = 1e-4, grayed = True, s = 0)
+    #model = Run((32,32), 10, 1500, alpha = 3e-4, grayed = True, s = 1)
+    part9((32,32))
 
---
+++ assignment2/part8/myalexnet.py
@@ -0,0 +1,175 @@
+
+import torch
+import torchvision.models as models
+import torchvision
+from torch.autograd import Variable
+
+import numpy as np
+import  matplotlib.pyplot as plt
+from scipy.misc import imread, imresize
+
+import torch.nn as nn
+
+
+def Resize(S, grayed = False):
+    '''
+    input: a turple of size. e.g. (32,32)
+    Resize the cropped images, and store them into XXX_size(_grayed)
+    hardcode_list is a list that contains filenames 
+    '''
+    hardcode_list = ['Kristin Chenoweth71.jpg']
+    for act_type in ['actors', 'actresses']:
+        if grayed:
+            store_path = dirpath + "/facescrub_%s_%i_grayed/" %(act_type, S[0])
+        else:
+            store_path = dirpath + "/facescrub_%s_%i/" %(act_type, S[0])
+        os.makedirs(store_path)
+        dataset_path = dirpath + "/facescrub_" + act_type + "_cropped/"
+        for root, dirs, files in os.walk(dataset_path):
+            dirs.sort()
+            files.sort()
+            for filename in files:
+                if filename not in hardcode_list:
+                    im = imread(dataset_path + filename, mode='RGB')
+                    resized = imresize(im, S)
+                    if grayed:
+                        if im.ndim == 3:
+                            resized = rgb2gray(resized)
+                        elif im.ndim == 2:
+                            resized = resized/255.
+                        else:
+                            continue
+                        plt.imsave(store_path + filename, resized, cmap = cm.gray)
+                    else:
+                        resized = resized/255.
+                        plt.imsave(store_path + filename, resized)
+
+
+def get_data(S, act, grayed = False, s = 0):
+    act_rawdata = {}
+    act_data = {}
+    for i in act:
+        act_rawdata[i] = np.empty((S[0]*S[0],0))
+        act_data[i] = [0, 0, 0]
+
+    for act_type in ['actors', 'actresses']:
+        if grayed:
+            dataset_path = dirpath + "/facescrub_%s_%i_grayed/" %(act_type, S[0])
+        else:
+            dataset_path = dirpath + "/facescrub_%s_%i/" %(act_type, S[0])
+        for root, dirs, files in os.walk(dataset_path):
+            dirs.sort()
+            files.sort()
+            for filename in files:
+                im = imread(dataset_path + filename)
+                im = im/255.
+                im = np.array([im[:,:,0].flatten()]).T
+                if np.amax(im) > 1.0 or np.amin(im) < 0. or np.isnan(im).any() or np.isinf(im).any():
+                    continue
+                else:
+                    for i in act:
+                        if i in filename:
+                            act_rawdata[i] = np.hstack((act_rawdata[i], im))
+
+    # randomly shuffle act_rawdata
+    np.random.seed(s)
+    for i in act:
+        act_rawdata[i] = act_rawdata[i][:,np.random.permutation(act_rawdata[i].shape[1])]
+
+    # act_data is a dictionary whose key is the name of actor/actress, value is the 3-element long array that the first
+    # element is a array that contains the training set, second one contains validation set, third one
+    # contains test set.
+    for i in act:
+        act_data[i][0] = act_rawdata[i][:, :min(70, act_rawdata[i].shape[1] - 30)]
+        act_data[i][1] = act_rawdata[i][:, -30:-20]
+        act_data[i][2] = act_rawdata[i][:, -20:]
+    return act_data
+
+
+
+
+
+# a list of class names
+from caffe_classes import class_names
+
+# We modify the torchvision implementation so that the features
+# after the final pooling layer is easily accessible by calling
+#       net.features(...)
+# If you would like to use other layer features, you will need to
+# make similar modifications.
+class MyAlexNet(nn.Module):
+    def load_weights(self):
+        an_builtin = torchvision.models.alexnet(pretrained=True)
+        
+        features_weight_i = [0, 3, 6, 8, 10]
+        for i in features_weight_i:
+            self.features[i].weight = an_builtin.features[i].weight
+            self.features[i].bias = an_builtin.features[i].bias
+            
+        classifier_weight_i = [1, 4, 6]
+        for i in classifier_weight_i:
+            self.classifier[i].weight = an_builtin.classifier[i].weight
+            self.classifier[i].bias = an_builtin.classifier[i].bias
+
+    def __init__(self, num_classes=1000):
+        super(MyAlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        
+        self.load_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+# model_orig = torchvision.models.alexnet(pretrained=True)
+model = MyAlexNet()
+model.eval()
+
+# Read an image
+im = imread('kiwi227.png')[:,:,:3]
+im = im - np.mean(im.flatten())
+im = im/np.max(np.abs(im.flatten()))
+im = np.rollaxis(im, -1).astype(float32)
+
+# turn the image into a numpy variable
+im_v = Variable(torch.from_numpy(im).unsqueeze_(0), requires_grad=False)    
+
+# run the forward pass AlexNet prediction
+softmax = torch.nn.Softmax()
+all_probs = softmax(model.forward(im_v)).data.numpy()[0]
+sorted_ans = np.argsort(all_probs)
+
+for i in range(-1, -6, -1):
+    print("Answer:", class_names[sorted_ans[i]], ", Prob:", all_probs[sorted_ans[i]])
+
+ans = np.argmax(model.forward(im_v).data.numpy())
+prob_ans = softmax(model.forward(im_v)).data.numpy()[0][ans]
+print("Top Answer:", class_names[ans], "P(ans) = ", prob_ans)
+
+
+
